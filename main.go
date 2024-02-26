81a4dd033154e7f1ad5392268c2d8c03c5a1ef14
中須 かすみ <42112059+Nakasu-ksm@users.noreply.github.com>
2024-02-25 18:53:06 -0800

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"go_sdk/animetrace"
 	"os"
@@ -9,15 +8,15 @@ import (
 
 func main() {
 	worker := animetrace.API()
-	worker.SetMultiple(0)
+
 	worker.SetModel("anime_model_lovelive")
 	imageBytes, err := os.ReadFile("demo.png")
 	if err != nil {
 		panic("画像の読み込みに失敗しました！")
 	}
-	buffer := new(bytes.Buffer)
-	writer := worker.SetImage(buffer, imageBytes)
-	result := animetrace.Recognition(buffer, writer.FormDataContentType())
-	response := result.ConvertToJson()
-	fmt.Println(response.Data[0].Name)
+	worker.SetImage(imageBytes)
+	worker.SetMultiple(0)
+	worker.Recognition()
+	jsonReturn := worker.ConvertToJson()
+	fmt.Println(jsonReturn.Data[0].Name)
 }
