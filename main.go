88346ee3191a29f458d2bd93e5dbda8651a308f7
中須 かすみ <42112059+Nakasu-ksm@users.noreply.github.com>
2024-02-25 16:31:04 -0800

    Update main.go

--- main.go
+++ main.go
@@ -9,9 +9,9 @@ import (
 
 func main() {
 	var worker animetrace.Worker
-	worker = &animetrace.Params{}
+	worker = animetrace.API()
 	worker.SetMultiple(0)
-	worker.SetModel("anime")
+	worker.SetModel("anime_model_lovelive")
 	imageBytes, err := os.ReadFile("demo.png")
 	if err != nil {
 		panic("画像の読み込みに失敗しました！")
@@ -21,5 +21,4 @@ func main() {
 	result := animetrace.Recognition(buffer, writer.FormDataContentType())
 	response := result.ConvertToJson()
 	fmt.Println(response.Data[0].Name)
-
 }
