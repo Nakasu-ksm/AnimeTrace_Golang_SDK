87e99e89ddf1f73e02d48a3e10c19205e549cc55
中須 かすみ <42112059+Nakasu-ksm@users.noreply.github.com>
2024-02-25 22:24:41 -0800

    Update main.go

--- main.go
+++ main.go
@@ -4,15 +4,39 @@ import (
 	"fmt"
 	"go_sdk/animetrace"
 	"os"
+	"time"
 )
 
+type AIRecognitionError struct {
+	errorTitle string
+	timeNow    time.Time
+}
+
+func (e *AIRecognitionError) Error() string {
+	e.timeNow = time.Now()
+	return "エラー発生: " + e.errorTitle + "  At" + e.timeNow.String()
+}
+
+func catchError(err string) error {
+	return &AIRecognitionError{errorTitle: err}
+}
+
 func main() {
+
+	defer func() {
+		if rec := recover(); rec != nil {
+			strs, ok := rec.(string)
+			if ok {
+				fmt.Println(catchError(strs))
+			}
+
+		}
+	}()
 	worker := animetrace.API()
+
 	worker.SetMultiple(true)
 	worker.SetModel("anime_model_lovelive")
-
 	worker.SetAI(true)
-
 	imageBytes, err := os.ReadFile("demo.png")
 	if err != nil {
 		panic("画像の読み込みに失敗しました！")
